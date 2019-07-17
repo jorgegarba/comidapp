c45091f8c0def2e7d3159c30857d69b272b4323e
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-07-17 01:24:52 -0500

    google inicio de sesion

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 import { firebaseConfig } from "./variables.js";
 
+
 // let script = document.createElement("script");
 // script.setAttribute("src","https://www.gstatic.com/firebasejs/6.3.0/firebase-app.js");
 // document.querySelector("body").prepend(script);
 
 window.onload = () => {
 
-    
+
 
 
     // Iniciando la configuración de firebase con nuestra aplicación
@@ -16,6 +17,77 @@ window.onload = () => {
         // estamos en index.html
         $.notify("estamos en index.html", "info");
 
+        let iniciarSesionGoogle = (e)=>{
+            e.preventDefault();
+            var provider = new firebase.auth.GoogleAuthProvider();
+
+            firebase.auth().signInWithPopup(provider).then(function(result) {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+                // ...
+              }).catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                console.log(error.message);
+                
+              });
+
+        }
+        
+        let cerrarSesion = (e)=>{
+            e.preventDefault();
+            firebase.auth().signOut().then(()=>{
+                $.notify("Cerró Sesion =)","success");
+            })
+        }
+
+        let escucharCambioSesion = ()=>{
+            firebase.auth().onAuthStateChanged(function (user) {
+                if (user) {
+                    // User is signed in.
+                    var displayName = user.displayName;
+                    var email = user.email;
+                    var emailVerified = user.emailVerified;
+                    var photoURL = user.photoURL;
+                    var isAnonymous = user.isAnonymous;
+                    var uid = user.uid;
+                    var providerData = user.providerData;
+                    console.log(uid);
+                    $("#btnCerrarSesion").removeAttr("hidden");
+                    $("#btnIniciarSesion").attr("hidden",true);
+                    
+                } else {
+                    // User is signed out.
+                    // ...
+                    console.log("No hay sesion iniciada");
+                    $("#btnIniciarSesion").removeAttr("hidden");
+                    $("#btnCerrarSesion").attr("hidden",true);
+                    
+                    
+                }
+            });
+        }
+        
+        let iniciarSesion = (e) => {
+            e.preventDefault();
+            firebase.auth().signInWithEmailAndPassword($("#inputEmail").val().trim(), $("#inputPassword").val().trim()).catch(function (error) {
+                console.log(error);
+            });
+        }
+        
+        $("#btnIniciarSesion").click(iniciarSesion);
+        $("#btnCerrarSesion").click(cerrarSesion);
+        $("#btnIniciarSesionGoogle").click(iniciarSesionGoogle);
+
+        escucharCambioSesion();
+        
     }
     if (location.href.indexOf("platos") >= 0) {
         $('input[type="range"]').rangeslider({
@@ -25,7 +97,7 @@ window.onload = () => {
             // the polyfill also in Browsers which support
             // the native <input type="range"> element.
             polyfill: true,
-        
+
             // Default CSS classes
             rangeClass: 'rangeslider',
             disabledClass: 'rangeslider--disabled',
@@ -33,55 +105,55 @@ window.onload = () => {
             verticalClass: 'rangeslider--vertical',
             fillClass: 'rangeslider__fill',
             handleClass: 'rangeslider__handle',
-        
+
             // Callback function
-            onInit: function() {},
-        
+            onInit: function () { },
+
             // Callback function
-            onSlide: function(position, value) {},
-        
+            onSlide: function (position, value) { },
+
             // Callback function
-            onSlideEnd: function(position, value) {}
+            onSlideEnd: function (position, value) { }
         });
         // Creando una referencia inicial al nodo "platos"
         let refPlatos = firebase.database().ref("platos");
         /**
          * Funcion para crear un registro en la DB de firebase
          */
-        let createPlato = ()=>{
+        let createPlato = () => {
 
             /**
              * 1. obtener una nueva clave o primary para el 
              * registro que se va a insertar
              */
-             let key = refPlatos.push().key;
-             /**
-              * 2. Referenciar al nodo que lleva por nombre
-              * la clave generada en el paso 1
-              */
-             let referenciaKey = refPlatos.child(key);
-             /**
-              * 3. Asignar atributos al nodo referenciado en 
-              * el paso [2] a partir del metodo "set()"
-              */
-             referenciaKey.set({
-                 nombre:$("#inputNombre").val().trim(),
-                 calorias: $("#inputCalorias").val(),
-                 origen: $("#inputOrigen").val(),
-                 descripcion: $("#inputDescripcion").val()
-             }).then(()=>{
-                $.notify("Plato creado correctamente","success");
+            let key = refPlatos.push().key;
+            /**
+             * 2. Referenciar al nodo que lleva por nombre
+             * la clave generada en el paso 1
+             */
+            let referenciaKey = refPlatos.child(key);
+            /**
+             * 3. Asignar atributos al nodo referenciado en 
+             * el paso [2] a partir del metodo "set()"
+             */
+            referenciaKey.set({
+                nombre: $("#inputNombre").val().trim(),
+                calorias: $("#inputCalorias").val(),
+                origen: $("#inputOrigen").val(),
+                descripcion: $("#inputDescripcion").val()
+            }).then(() => {
+                $.notify("Plato creado correctamente", "success");
                 $("#modalCrearPlato").modal("hide");
-             }).catch( error =>{
-                $.notify("Error al crear el plato","danger");
+            }).catch(error => {
+                $.notify("Error al crear el plato", "danger");
                 console.log(error);
-             });
+            });
 
-        }  
+        }
 
         // configurando click al boton de agregar plato
         // para aparecer modal
-        $("#btnCrearPlato").click(()=>{
+        $("#btnCrearPlato").click(() => {
             $("#modalCrearPlato").modal("show")
         });
 
